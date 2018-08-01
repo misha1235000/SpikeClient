5db3fafd9fb992ae0627fffa58599c29b63ec817
Misha <misha1235000@gmail.com>
2018-08-01 13:18:49 +0300

    Added register service

---
+++ src/app/register/register.service.ts
@@ -0,0 +1,59 @@
+// verify.service
+
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+
+function getCookie(name) {
+  let ca: Array<string> = document.cookie.split(';');
+        let caLen: number = ca.length;
+        let cookieName = `${name}=`;
+        let c: string;
+
+        for (let i: number = 0; i < caLen; i += 1) {
+            c = ca[i].replace(/^\s+/g, '');
+            if (c.indexOf(cookieName) == 0) {
+                return c.substring(cookieName.length, c.length);
+            }
+        }
+        return '';
+}
+
+@Injectable()
+export class RegisterService {
+  private serverUrl = "http://localhost:3000/api/auth";
+
+  /**
+   * Injection of the http service.
+   * @param http
+   */
+  constructor(private http: Http){}
+
+  login(email, password): Observable<any> {
+     return this.http.post(this.serverUrl + '/login', {'email': email, 'password': password})
+            .map((data) => {
+                return data.json();
+              });
+  }
+
+  isExist(): Observable<any> {
+    return this.http.get(this.serverUrl + '/logout')
+           .map((data) => {
+              return data.json();
+           });
+  }
+
+  /*
+  verifyCode(code, reqId, appointment, user): Observable<any> {
+    return this.http.post(this.serverUrl + '/verify', {'code': code, 'request_id': reqId, 'appointment': appointment, 'user': user})
+           .map((res: Response) => {
+              return res.json();
+           }).catch((error) => {
+              return error.json().error;
+           });
+  }*/
+
+}
