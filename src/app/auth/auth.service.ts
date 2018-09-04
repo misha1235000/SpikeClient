5d4e48105308ec6e9e508f10757bc89039847876
Misha <misha1235000@gmail.com>
2018-09-04 16:01:39 +0300

    refactor: Refactor of all the structure.

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,71 @@
+// auth.service
+
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { PublicFunctions } from '../shared/shared';
+
+@Injectable()
+export class AuthService {
+  private authUrl = 'http://localhost:3000/api/auth';
+  private teamUrl = 'http://localhost:3000/api/team';
+  private clientUrl = 'http://localhost:3000/api/client';
+
+  /**
+   * Injection of the http service.
+   * @param http - The http service.
+   */
+  constructor(private http: Http) {}
+
+  /**
+   * Gets a username by the token which is saved on the cookie.
+   */
+  getTeamName(): Observable<any> {
+    const headers = new Headers();
+
+    headers.append('authorization', PublicFunctions.getCookie('token'));
+
+    return this.http.get(this.teamUrl, {headers})
+           .map((data) => {
+             return data.json();
+           }).catch((error) => { // If there is any error.
+             if (error.status === 401) { // If the error's status is 401 (Unauthorized), then logout.
+                PublicFunctions.logout();
+             }
+
+             return Observable.throw(error.json());
+           });
+  }
+
+  /**
+   * Register a new team account.
+   * @param team - The team object to register.
+   */
+  registerTeam(team): Observable<any> {
+    const headers = new Headers();
+
+    headers.append('authorization', PublicFunctions.getCookie('token'));
+
+    return this.http.post(this.teamUrl, {'team': team})
+            .map((data) => {
+                return data.json();
+            }).catch((error) => {
+                return Observable.throw(error.json());
+            });
+  }
+
+  /**
+   * Login with http service.
+   * @param team - The team object with login details.
+   */
+  login(team): Observable<any> {
+    return this.http.post(this.authUrl + '/login', {'team': team})
+           .map((data) => {
+               return data.json();
+             }).catch((error) => {
+               return Observable.throw(error.json());
+             });
+ }
+}
---
+++ src/app/auth/open-register-team/open-register-team.component.ts
@@ -0,0 +1,61 @@
+// open-register.component
+
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '../../../../node_modules/@angular/material';
+import { PublicFunctions } from '../../shared/shared';
+
+@Component({
+  selector: 'app-open-register',
+  templateUrl: './open-register-team.component.html',
+  styleUrls: ['./open-register-team.component.css']
+})
+export class OpenRegisterTeamComponent implements OnInit {
+  isLogged: boolean;
+  teamName: string;
+  password: string;
+  website = '';
+  callback = '';
+  appName = '';
+  isDone = true;
+  AUTHORIZE_HELP = 'For use with requests from a web server. This is the path' +
+                   'in your application that users are redirected to after they' +
+                   'have authenticated with Google. The path will be appended with the' +
+                   'authorization code for access. Must have a protocol.' +
+                   'Cannot contain URL fragments or relative paths. Cannot be a public IP address.';
+  ORIGIN_HELP = 'For use with requests from a browser. This is the origin URI of the client' +
+                'application. It can\'t contain a wildcard (https://*.example.com) or a path' +
+                '(https://example.com/subdir). If you\'re using a nonstandard port, you must include it in the origin URI.';
+
+
+  /**
+   * Inits the needed services.
+   * @param openLoginService - The open login service.
+   * @param dialogRef - The dialog ref service.
+   * @param data - The data service.
+   */
+  constructor(public dialogRef: MatDialogRef<OpenRegisterTeamComponent>,
+  @Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  /**
+   * Checks whether the team account is logged in or not.
+   */
+  checkLogin(): void {
+    if (PublicFunctions.getCookie('token').length > 0) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+      document.cookie = 'token=;expires=;Thu, 01 Jan 1970 00:00:01 GMT;';
+      window.location.href = '/login';
+    }
+  }
+
+  ngOnInit() {
+  }
+
+  /**
+   * Closes the dialog.
+   */
+  close(): void {
+    this.dialogRef.close();
+  }
+}
