5a20618138a7485f0f60cd6edf194e1596e5d557
Misha <misha1235000@gmail.com>
2018-09-16 13:46:59 +0300

    feat: Added real tokens display from the server. Closes #28

---
+++ src/app/tokens/tokens.service.ts
@@ -0,0 +1,40 @@
+// tokens.service
+
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { PublicFunctions } from '../shared/shared';
+
+@Injectable()
+export class TokensService {
+  private clientUrl = 'http://localhost:3000/api/client';
+
+  /**
+   * Injection of the http service.
+   * @param http - The http service.
+   */
+  constructor(private http: Http) {}
+
+  /**
+   * Gets a username by the token which is saved on the cookie.
+   */
+  getTokens(): Observable<any> {
+    const headers = new Headers();
+
+    headers.append('authorization', PublicFunctions.getCookie('token'));
+
+    return this.http.get(this.clientUrl, {headers})
+           .map((data) => {
+             return data.json();
+           }).catch((error) => { // If there is any error.
+             if (error.status === 401) { // If the error's status is 401 (Unauthorized), then logout.
+                PublicFunctions.logout();
+             }
+
+             return Observable.throw(error.json());
+           });
+  }
+}
