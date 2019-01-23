a4279fda3069eede02e95b8cb7d128f4a232a0d0
Misha <misha1235000@gmail.com>
2019-01-23 12:01:41 +0200

    feat: Adding under-construction page for uncompleted pages

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,7 +1,9 @@
 // config
 
 export const config = {
-    SERVER_HOST: 'http://40.68.41.239',
+
+//  SERVER_HOST: 'http://40.68.41.239',
+    SERVER_HOST: 'http://localhost',
     SERVER_PORT: '3000',
     CLIENT_PORT: '4200',
 };
