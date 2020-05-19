f4a5b6b95124483ab2939a29d58517d52f60ca8c
root <root@spike.jblbbw2jjb1ehpytxsjmbwaqqc.ax.internal.cloudapp.net>
2020-05-19 07:41:55 +0000

    fix: CSS fixes, and aligned to rtl the autocomplete.

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -2,10 +2,10 @@
 
 export const config = {
 
-        SERVER_HOST: 'https://localhost',
-    //    SERVER_HOST: 'https://51.144.178.121',
-        SERVER_PORT: '3002',
-        CLIENT_PORT: '443',
+    //    SERVER_HOST: 'https://localhost',
+        SERVER_HOST: 'https://51.144.178.121',
+        SERVER_PORT: '3005',
+        CLIENT_PORT: '4200',
         PRIVATE_KEY_PATH: 'src/certs/files/privatekey.pem',
         CERT_PATH: 'src/certs/files/certificate.pem',
         ELASTIC_APM_SERVICE_NAME: 'apm_service',
